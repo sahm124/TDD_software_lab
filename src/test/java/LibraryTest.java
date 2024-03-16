658fe5aa2ab9dd88b24064d71b1ddaad0037dcd9
MJAlaeddini <javad1379.a@gmail.com>
2024-03-16 23:47:00 +0330

    Handle commit

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,3 +1,17 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
-publi
+public class LibraryTest {
+    
+    @Test
+    public void testLendBookToNonRegisteredStudent() {
+        Library library = new Library();
+        Book book = new Book("Book-1", "Author-1", 1);
+        Student student = new Student("Student-1", 1);
+        
+        library.addBook(book);
+        
+        boolean result = library.lendBook(book, student);
+        Assert.assertFalse("Should not lend book to non-registered student", result);
+    }
+}
